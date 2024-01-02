666aa0ccddc7eaa3c7afe955d917d0d728f4856e
onestone23 <lws2269@kakao.com>
2024-01-02 11:44:10 +0900

    test 실패케이스

--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,20 +1,30 @@
 import { logRoles } from '@testing-library/dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 /**
  * 역할(role)에 대한 링크
  * https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/link_role
  */
-test('초기 버튼 색깔이 빨간색인지', () => {
-  const { container } = render(<App />);
-  // 아래 구문을 통해 컴포넌트를 가져올 수 있다? 더 찾아봐야할듯
-  logRoles(container);
-  // 버튼 텍스트에 대해 아래와 같이 사용도 가능
-  const button = screen.getByRole('button', { name: /blue/i });
-  // const button = screen.getByRole('button');
+test('버튼 클릭 흐름', () => {
+  /**
+    아래 구문을 통해 컴포넌트를 가져올 수 있다? 더 찾아봐야할듯
+    const { container } = render(<App />);
+    logRoles(container);
+
+    버튼 텍스트에 대해 아래와 같이 사용도 가능
+    const button = screen.getByRole('button', { name: /blue/i });
+   */
+  // 앱 렌더
+  render(<App />);
+  // 버튼 찾기
+  const button = screen.getByRole('button');
+  // 클릭 전 컬러, 텍스트 확인
   expect(button).toHaveClass('red');
+  expect(button).toHaveTextContent(/blue/i);
+  // 버튼 클릭
+  fireEvent.click(button);
+  // 클릭 후 버튼 색상 및 텍스트 확인
+  expect(button).toHaveClass('blue');
+  expect(button).toHaveTextContent(/red/i);
 });
-test('초기 버튼 텍스트', () => {});
-test('클릭 후 버튼 색깔', () => {});
-test('클룩 후 버튼 텍스트', () => {});
