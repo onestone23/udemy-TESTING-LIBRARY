d28b576724b3e466ce06cd08e313551cd0ace01f
onestone23 <lws2269@kakao.com>
2024-01-02 15:17:10 +0900

    간단한 버튼,체크박스  클릭 테스트

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -1,9 +1,33 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [buttonColor, setButtonColor] = useState('red');
+  const [disabled, setDisabled] = useState(false);
+  const nextColor = buttonColor === 'red' ? 'blue' : 'red';
+  const className = disabled ? 'gray' : buttonColor;
+
   return (
     <div>
-      <button className='red'>Change to blue</button>
+      <button
+        className={className}
+        onClick={(e) => {
+          setButtonColor(nextColor);
+        }}
+        disabled={disabled}
+      >
+        Change to {nextColor}
+      </button>
+      <br />
+      <input
+        type='checkbox'
+        id='disabled-button-checkbox'
+        defaultChecked={disabled}
+        onChange={(e) => {
+          setDisabled(e.target.checked);
+        }}
+      />
+      <label htmlFor='disabled-button-checkbox'>Disable button</label>
     </div>
   );
 }
--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -3,6 +3,8 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 /**
+ *
+ * https://github.com/testing-library/jest-dom
  * 역할(role)에 대한 링크
  * https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/link_role
  */
@@ -26,5 +28,44 @@ test('버튼 클릭 흐름', () => {
   fireEvent.click(button);
   // 클릭 후 버튼 색상 및 텍스트 확인
   expect(button).toHaveClass('blue');
+  // 아래와 같이 스타일을 직접 테스트 할 수 있지만, blue의 경우 rgb값을 작성해줘야하고, 여러가지 문제때문에 스타일을 직접 테스트 하는것보다 클래스를 통한 검증을 선호
+  // expect(button).toHaveStyle({'background-color': 'blue'});
   expect(button).toHaveTextContent(/red/i);
+});
+
+test('체크박스 클릭시 버튼 색깔에 대한 흐름', () => {
+  render(<App />);
+  const button = screen.getByRole('button', { name: /blue/i });
+  const checkbox = screen.getByRole('checkbox', { name: /disable button/i });
+
+  // 초기 상태 확인
+  expect(button).toBeEnabled();
+  expect(checkbox).not.toBeChecked();
+
+  // 체크박스 클릭
+  fireEvent.click(checkbox);
+  // 체크박스 클릭 후 상태
+  expect(button).toBeDisabled();
+  expect(button).toHaveClass('gray');
+
+  // 체크박스 클릭
+  fireEvent.click(checkbox);
+  // 체크박스 클릭 후 상태
+  expect(button).toBeEnabled();
+  expect(button).toHaveClass('red');
+});
+
+test('체크박스 클릭시 버튼의 대한 상태에 대한 흐름', () => {
+  render(<App />);
+  const button = screen.getByRole('button', { name: /blue/i });
+  const checkbox = screen.getByRole('checkbox', { name: /disable button/i });
+
+  fireEvent.click(button);
+
+  // 체크박스 클릭
+  fireEvent.click(checkbox);
+  expect(button).toBeDisabled();
+
+  fireEvent.click(checkbox);
+  expect(button).toBeEnabled();
 });
