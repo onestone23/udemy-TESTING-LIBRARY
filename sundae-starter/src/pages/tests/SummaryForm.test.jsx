5149a212fd2a89eb1951801119d55cf9d7b7b1dc
onestone23 <lws2269@kakao.com>
2024-01-03 15:02:16 +0900

    hover test

--- sundae-starter/src/pages/tests/SummaryForm.test.jsx
+++ sundae-starter/src/pages/tests/SummaryForm.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import { SummaryForm } from '../summary/SummaryForm';
+import userEvent from '@testing-library/user-event';
 
 test('초기 세팅', () => {
   render(<SummaryForm />);
@@ -11,15 +12,41 @@ test('초기 세팅', () => {
   expect(button).toBeDisabled();
 });
 
-test('체크박스 클릭 시 버튼 활성화에 대한 흐름', () => {
+test('체크박스 클릭 시 버튼 활성화에 대한 흐름', async () => {
+  const user = userEvent.setup();
+
   render(<SummaryForm />);
 
   const checkbox = screen.getByRole('checkbox', /terms and coditins/i);
   const button = screen.getByRole('button', /confirm order/i);
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(button).toBeEnabled();
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(button).toBeDisabled();
+});
+
+// https://testing-library.com/docs/react-testing-library/cheatsheet
+test('팝 오버 흐름', async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+
+  // 쿼리 - 요소가 DOM 내에 있지 않을 것
+  // get - 요소가 있을 것
+  // find - 요소가 비동기적으로 나타날 때
+
+  // 팝오버 숨김 확인
+  const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i);
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // 마우스 호버
+  const termsAndConditions = screen.getByText(/terms and condition/i);
+  await user.hover(termsAndConditions);
+  const popover = screen.queryByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  // 마우스 호버 끝
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
 });
--- sundae-starter/vite.config.js
+++ sundae-starter/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,8 +7,8 @@ export default defineConfig({
   test: {
     // https://github.com/vitest-dev/vitest/blob/main/examples/react-testing-lib/vite.config.ts
     globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.js",
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
     css: true,
